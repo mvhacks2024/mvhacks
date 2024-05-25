41ecc154186887fa9b44abc79db7c92e8ce3cd55
Rohit Falor <rohitfalor@outlook.com>
2024-05-25 00:50:24 -0700

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<div class=" bg-green-100 flex justify-center py-10">
+    <a href="/about" class="text-green-800 text-6xl font-serif font-bold">
+        Home
+    </a>
+</div>
+
+<div class="flex justify-center mt-12">
+    <h1 class="text-green-950 text text-3xl font-mono font-semibold">
+        Hi! My name is Raima Falor :]
+    </h1>
+</div>
+
+<div class="flex mt-12 justify-evenly items-center flex-wrap">
+    <div class="flex font-mono mt-6 ml-20 text-[24px] max-w-lg">
+        <p>
+            Hello I am a senior at Monta Vista High School, and soon to be at
+            Santa Clara University. I am currently a Computer Science major and
+            I have a passion for web development. I am also a big fan of the
+            music genres of indie pop and hip hop. I love to travel and explore
+            new places. I also enjoy playing video games, baking, making
+            puzzles, and spending time with my friends.
+        </p>
+    </div>
+
+    <div class="flex justify-end align-center w-fit">
+        <img
+            class="w-96 h-96"
+            src="https://media.licdn.com/dms/image/D5603AQG2CvoYkVrdng/profile-displayphoto-shrink_800_800/0/1696047955770?e=1721865600&v=beta&t=CQqfJRkZ7yYcB6uZ4lMho3_OJbM3Gj07VpIFOg-3YAs"
+            alt=""
+        />
+    </div>
+</div>
+
+<style>
+</style>
